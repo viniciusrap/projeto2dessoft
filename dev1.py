cf040af03f145d788e2da86841513e4027c15c89
Vinicius R <viniciusrap@al.insper.edu.br>
2023-04-26 14:25:21 -0300

    vini - preenche_frota

--- dev1.py
+++ dev1.py
@@ -8,3 +8,32 @@ def define_posicoes(linha,coluna,orientação,tamanho):
             posição.append([linha,(coluna+i)])
         i += 1
     return posição
+
+def preenche_frota(frota,nome_navio,linha,coluna,orientação,tamanho):
+    novo_navio = {}
+    if len(frota) > 0:
+        p = 0
+        lista = []
+        for x in frota:
+            if x == nome_navio:
+                p = 1
+                for x  in frota:
+                    if x == nome_navio:
+                        novo = define_posicoes(linha,coluna,orientação,tamanho)
+                        for t in frota[x]:
+                            antigo = t 
+                            lista.append(antigo)
+                        lista.append(novo)
+                        novo_navio[nome_navio] = lista
+    if len(frota) > 0 and p == 0 :
+        novo = define_posicoes(linha,coluna,orientação,tamanho)
+        novo_navio[nome_navio] = [novo]
+
+    elif len(frota) == 0:
+        novo = define_posicoes(linha,coluna,orientação,tamanho)
+        novo_navio[nome_navio] = [novo]
+        
+    for y in frota:
+        if y != nome_navio:
+            novo_navio[y] = frota[y]
+    return novo_navio
