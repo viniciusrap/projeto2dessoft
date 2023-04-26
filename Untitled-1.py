6bb056da0cab14f9e082de36e6206f2936f381db
Vinicius R <viniciusrap@al.insper.edu.br>
2023-04-26 13:12:28 -0300

    vini - define_posicoes

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,18 @@
-print('vini')
 def define_posicoes(linha,coluna,orientação,tamanho):
     posição = []
     i = 0 
-    while i < tamanho:
+    while i < len(tamanho):
         if orientação == 'vertical':
             posição.append([(linha+i),coluna])
         if orientação == 'horizontal':
             posição.append([linha,(coluna+i)])
         i += 1
     return posição
+
+
+
+    
+
+
+
+  
